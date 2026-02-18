128a468cfb9a373a62fa0da7606a88aad3dd2f61
Ariel Marcelo Pardo <adoorno@gmail.com>
2026-02-17 21:31:07 -0300

    Add, finally some Corutines and Channels.

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -11,11 +11,15 @@ type Sleeper interface {
 	Sleep()
 }
 
-type DefaultSleeper struct {}
-
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1*time.Second)
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
 }
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
 
 const finalWord = "go!"
 const countdownStart = 3
@@ -29,6 +33,6 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }

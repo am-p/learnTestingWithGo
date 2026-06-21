062fd967e2f977d537b248898b0475f4aca7a35e
Ariel Marcelo Pardo <arielmp@localhost.localdomain>
2026-06-21 12:55:23 -0300

    Adding sync: mutex rules!

--- sync/sync.go
+++ sync/sync.go
@@ -1,11 +1,21 @@
 package sync
 
+import (
+       "sync"
+)
 type Counter struct {
-	value int
+     mu sync.Mutex
+     value int
+}
+
+func NewCounter() *Counter {
+     return &Counter{}
 }
 
 func (c *Counter) Inc() {
-	c.value++
+     c.mu.Lock()
+     defer c.mu.Unlock()
+     c.value++
 }
 
 func (c *Counter) Value() int {
